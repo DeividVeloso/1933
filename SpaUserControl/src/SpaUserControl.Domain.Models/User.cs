583aec8e372483c34f0b982b08328f79df37fada
Deivid Higino Veloso <dveloso@acessocard.com.br>
2016-12-21 09:15:20 -0200

    Repository

---
+++ SpaUserControl/src/SpaUserControl.Domain.Models/User.cs
@@ -0,0 +1,68 @@
+﻿using SpaUserControl.Common.Resources;
+using SpaUserControl.Common.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.Domain.Models
+{
+    public class User
+    {
+        #region Ctor
+        public User(string name, string email, string password)
+        {
+            //Gerando um ID automatico
+            this.Id = Guid.NewGuid();
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+        }
+        #endregion
+
+        #region properties
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        #endregion
+
+        #region methods
+        public void SetPassword(string password, string confirmaPassword)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, Errors.InvalidPassword);
+            AssertionConcern.AssertArgumentNotNull(confirmaPassword, Errors.InvalidPasswordConfirmation);
+            AssertionConcern.AssertArgumentEquals(password, confirmaPassword, Errors.PasswordNotMatch);
+            AssertionConcern.AssertArgumentLength(password, 6, 20, Errors.InvalidPassword);
+
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+        }
+
+        public void ChangeName(string name)
+        {
+            this.Name = name;
+        }
+
+        public string ResetPassword()
+        {
+            string password = Guid.NewGuid().ToString().Substring(0, 8);
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+            return password;
+        }
+
+        public void Validate()
+        {
+            AssertionConcern.AssertArgumentLength(this.Name, 3, 250, Errors.InvalidUserName);
+            EmailAssertionConcern.AssertIsValid(this.Email);
+            PasswordAssertionConcern.AssertIsValid(this.Password);
+        }
+
+
+        public void Celular()
+        {
+            
+        }
+        #endregion
+    }
+}
